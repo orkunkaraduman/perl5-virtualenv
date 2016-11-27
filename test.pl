6d487709910145bb758d78229dbfa7d992724797
Orkun Karaduman <orkunkaraduman@gmail.com>
2016-11-27 18:39:48 +0300

    test.pl

--- test.pl
+++ test.pl
@@ -6,13 +6,8 @@ no warnings qw(qw utf8);
 use v5.10;
 use open qw(:std :locale);
 use utf8;
-use Perl::Shell;
-use POSIX qw(:locale_h);
-use locale;
 
-use lib './lib';
 use App::virtualenv;
 
-setlocale LC_ALL, 'en_US.UTF-8';
 
 exit App::virtualenv::shell(undef, @ARGV);
