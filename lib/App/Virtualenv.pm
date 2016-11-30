2268b20086397cb568e118c047a2a8dbcb0f1347
Orkun Karaduman <orkunkaraduman@gmail.com>
2016-11-30 16:57:35 +0300

    removed shell.pl fix

--- lib/App/Virtualenv.pm
+++ lib/App/Virtualenv.pm
@@ -139,7 +139,6 @@ sub create
 	_system("cp -v $pkgPath/Virtualenv/activate $virtualenvPath/bin/activate && chmod 644 $virtualenvPath/bin/activate");
 	_system("cp -v $pkgPath/Virtualenv/sh.pl $virtualenvPath/bin/sh.pl && chmod 755 $virtualenvPath/bin/sh.pl");
 	_system("cp -v $pkgPath/Virtualenv/perl.pl $virtualenvPath/bin/perl.pl && chmod 755 $virtualenvPath/bin/perl.pl");
-	_system("cp -v $pkgPath/Virtualenv/shell.pl $virtualenvPath/bin/shell.pl && chmod 755 $virtualenvPath/bin/shell.pl");
 
 	return 1;
 }
