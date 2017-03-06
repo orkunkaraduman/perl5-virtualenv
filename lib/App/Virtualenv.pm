d0a998f5d8511b41e5e70a35b5b2bb6284708da0
Orkun Karaduman <orkunkaraduman@gmail.com>
2017-03-06 16:41:16 +0300

    v2.03 init

--- lib/App/Virtualenv.pm
+++ lib/App/Virtualenv.pm
@@ -5,7 +5,7 @@ App::Virtualenv - Perl virtual environment
 
 =head1 VERSION
 
-version 2.02
+version 2.03
 
 =head1 ABSTRACT
 
@@ -37,7 +37,7 @@ use Lazy::Utils;
 BEGIN
 {
 	require Exporter;
-	our $VERSION     = '2.02';
+	our $VERSION     = '2.03';
 	our @ISA         = qw(Exporter);
 	our @EXPORT      = qw(main run);
 	our @EXPORT_OK   = qw();
